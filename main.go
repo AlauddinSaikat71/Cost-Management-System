4a46e9f012695f830de2e60cdd3688d6d470172e
AlauddinSaikat71 <alauddin27@student.sust.edu>
2021-05-10 12:54:06 +0600

    Cors middleware method update

--- main.go
+++ main.go
@@ -4,13 +4,28 @@ import (
 	"costmanagement/controllers"
 	"costmanagement/db"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000/")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	r := gin.Default()
-	r.Use(cors.Default())
+	CORSMiddleware()
 
 	db.ConnectDatabase()
 	db.InitModelDbContext()
@@ -22,4 +37,6 @@ func main() {
 	r.DELETE("/users/:id", controllers.DeleteUser)
 
 	r.Run()
+	// listen and server the router with the port
+	//log.Fatal(r.Run(":3000"))
 }
